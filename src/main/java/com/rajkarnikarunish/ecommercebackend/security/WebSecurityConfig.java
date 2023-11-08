ed4952450aa399d032b8af2463e832ee320f340e
Unish Rajkarnikar <ubidy947@gmail.com>
2023-11-07 22:29:12 -0600

    test: authentication controller test->vid13

--- src/main/java/com/rajkarnikarunish/ecommercebackend/security/WebSecurityConfig.java
+++ src/main/java/com/rajkarnikarunish/ecommercebackend/security/WebSecurityConfig.java
@@ -20,7 +20,7 @@ public class WebSecurityConfig {
         http.csrf().disable().cors().disable(); //cors allows requests from the authorized domains
         http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
         http.authorizeHttpRequests()
-                .requestMatchers("/product", "/auth/register", "/auth/login", "/auth/verify").permitAll()
+                .requestMatchers("/product", "/auth/register", "/auth/login", "/auth/verify", "/error").permitAll()
                 .anyRequest().authenticated();
         return http.build();
     }
---
+++ src/test/java/com/rajkarnikarunish/ecommercebackend/api/controller/product/ProductTest.java
@@ -0,0 +1,24 @@
+package com.rajkarnikarunish.ecommercebackend.api.controller.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc //Tells spring I want use mvc framework for unit testing, we need to autowire mock mvc object
+public class ProductTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testProductList() throws Exception {
+        mvc.perform(get("/product")).andExpect(status().is(HttpStatus.OK.value()));
+    }
+
+}
